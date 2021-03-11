58322efc00b43e2abdca3792a638a58ccffb8546
Fess00 <ss_s0l0@mail.ru>
2021-03-11 14:51:12 +0300

    Completed with passing tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+    let buf;
+    n += '';
+    n = n.split('');
+    for (let i = 0; i < Math.floor(n.length/2); ++i) {
+        buf = n[n.length - i - 1];
+        n[n.length - i - 1] = n[i];
+        n[i] = buf;
+    }
+    n = n.join('');
+    return Number(n);
 }
